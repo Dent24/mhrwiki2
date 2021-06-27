8d45f3ce191075dc7c111a3121c8d886341d835c
Dent24 <cljhs90607@gmail.com>
2021-06-27 18:03:42 +0800

    [修改]新增資料頁面-部位與肉質

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # mhrwiki2
 魔物獵人 崛起 - 頁面改版
+
+## 網址
+https://dent24.github.io/mhrwiki2/
+
+## 版本說明
+
+### 改版內容
+- UI更新
+- 新增 部位與肉質 資訊
+
+### 預期改版
+- 新增 素材 資訊
+- 新增 小型魔物 頁面
+
+### 技術使用
+- 前端: Vue(CDN引入), Vuex(CDN引入)
+- 資料庫: Google Sheet
+- API: Google Apps Script
+
+### 資料庫(Google Sheet, 僅能檢視)
+https://docs.google.com/spreadsheets/d/1araK8RBdQljwikCSTV4O87HfaOnwu4II5-nurvemVxs/edit?usp=sharing
+
+### 資源
+- UI參考: https://www.pinterest.com/pin/578853358359612345/
+- 圖源: 魔物獵人崛起截圖
--- js/main.js
+++ js/main.js
@@ -3,40 +3,26 @@ const store = new Vuex.Store({
     state: {
         dictionary: {},
         monster: {},
-        weakness: {}
+        weakness: {},
+        fleshy: {}
     },
     getters: {
-        getDictionary: state => {
-            return state.dictionary;
-        },
-        getMonster: state => {
-            return state.monster;
-        },
-        getWeakness: state => {
-            return state.weakness;
-        }
+        getDictionary: state => { return state.dictionary; },
+        getMonster: state => { return state.monster; },
+        getWeakness: state => { return state.weakness; },
+        getFleshy: state => { return state.fleshy; }
     },
     mutations: {
-        mutationDictionary (state, obj) {
-            state.dictionary = obj;
-        },
-        mutationMonster (state, obj) {
-            state.monster = obj;
-        },
-        mutationWeakness (state, obj) {
-            state.weakness = obj;
-        }
+        mutationDictionary (state, obj) { state.dictionary = obj; },
+        mutationMonster (state, obj) { state.monster = obj; },
+        mutationWeakness (state, obj) { state.weakness = obj; },
+        mutationFleshy (state, obj) { state.fleshy = obj; }
     },
     actions: {
-        actionDictionary ({commit}, obj) {
-            commit('mutationDictionary', obj);
-        },
-        actionMonster ({commit}, obj) {
-            commit('mutationMonster', obj);
-        },
-        actionWeakness ({commit}, obj) {
-            commit('mutationWeakness', obj);
-        }
+        actionDictionary ({ commit }, obj) { commit('mutationDictionary', obj); },
+        actionMonster ({ commit }, obj) { commit('mutationMonster', obj); },
+        actionWeakness ({ commit }, obj) { commit('mutationWeakness', obj); },
+        actionFleshy ({ commit }, obj) { commit('mutationFleshy', obj); }
     }
 })
 
@@ -45,14 +31,19 @@ const main = new Vue({
     el: '#main',
     data() {
         return {
-            msg: 'hello',
-            nowMonster: '1'
+            nowMonster: '1',
+            contentList: {
+                detail: 'S_MEN_DET',
+                fleshy: 'S_MEN_FLE'
+            },
+            nowContent: 'detail'
         }
     },
     created() {
+        const url = 'https://script.google.com/macros/s/AKfycbzTN0YUTLoxxH4lLXjqaX6TKOeTV7Hd7x_xED_KbRuNI8VNV5N_vywDKuWKx0ees6jlEA/exec';
         // 字典檔載入
         axios
-            .get('https://script.google.com/macros/s/AKfycbzTN0YUTLoxxH4lLXjqaX6TKOeTV7Hd7x_xED_KbRuNI8VNV5N_vywDKuWKx0ees6jlEA/exec?type=dictionary')
+            .get(`${url}?type=dictionary`)
             .then((res) => {
                 const objBack = {};
                 for (var i = 1; i < res.data.length; i++) {
@@ -63,7 +54,7 @@ const main = new Vue({
 
         // 魔物列表
         axios
-            .get('https://script.google.com/macros/s/AKfycbzTN0YUTLoxxH4lLXjqaX6TKOeTV7Hd7x_xED_KbRuNI8VNV5N_vywDKuWKx0ees6jlEA/exec?type=monster')
+            .get(`${url}?type=monster`)
             .then((res) => {
                 const objBack = {};
                 for (var i = 1; i < res.data.length; i++) {
@@ -77,7 +68,7 @@ const main = new Vue({
 
         // 弱點列表
         axios
-            .get('https://script.google.com/macros/s/AKfycbzTN0YUTLoxxH4lLXjqaX6TKOeTV7Hd7x_xED_KbRuNI8VNV5N_vywDKuWKx0ees6jlEA/exec?type=weakness')
+            .get(`${url}?type=weakness`)
             .then((res) => {
                 const objBack = {};
                 for (var i = 1; i < res.data.length; i++) {
@@ -88,16 +79,47 @@ const main = new Vue({
                 }
                 store.dispatch('actionWeakness', objBack);
             });
+
+        // 肉質
+        axios
+            .get(`${url}?type=fleshy`)
+            .then((res) => {
+                const objBack = {
+                    weakness: res.data[0].slice(2)
+                };
+                for (var i = 1; i < res.data.length; i++) {
+                    if (!objBack[res.data[i][0]]) objBack[res.data[i][0]] = [];
+                    objBack[res.data[i][0]].push({
+                        dictionary: res.data[i][1],
+                        weakpoint: res.data[i].slice(2)
+                    });
+                }
+                store.dispatch('actionFleshy', objBack);
+            });
     },
     computed: {
-        dictionaryList() {
-            return store.getters.getDictionary;
+        dictionaryList() { return store.getters.getDictionary; },
+        monsterList() { return store.getters.getMonster; },
+        weaknessList() { return store.getters.getWeakness; },
+        fleshyList() { return store.getters.getFleshy; }
+    },
+    methods: {
+        itemClass(index) {
+            let classList = `list-item list-item${index}`;
+            if (this.nowMonster === index) classList += ' active';
+            return classList;
         },
-        monsterList() {
-            return store.getters.getMonster;
+        contentUp() {
+            const contentKey = Object.keys(this.contentList);
+            let nowKey = contentKey.indexOf(this.nowContent) - 1;
+            if (nowKey < 0) nowKey = contentKey.length - 1;
+            this.nowContent = contentKey[nowKey];
         },
-        weaknessList() {
-            return store.getters.getWeakness;
+        contentDown() {
+            const contentKey = Object.keys(this.contentList);
+            let nowKey = contentKey.indexOf(this.nowContent) + 1;
+            if (nowKey === contentKey.length) nowKey = 0;
+            this.nowContent = contentKey[nowKey];
         }
     }
 })
